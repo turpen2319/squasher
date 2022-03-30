96a3cbf74e7f801b5982cb8265baf95de0a55009
turpen2319 <austin-turpen@outlook.com>
2022-03-30 12:10:29 -0700

    fixes bugs in solved ui/logic & adds user authorization to front end

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,7 @@
 const $showQuestionKebab = $('#show-question-kebab');
 const $kebabMenuItems = $('.kebab-menu-items')
 
+
 $showQuestionKebab.click(function() {
     
     $kebabMenuItems.toggleClass("show-menu")
