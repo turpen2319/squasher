46690143f2d12c0937ffce34d7175700c3ec0472
turpen2319 <austin-turpen@outlook.com>
2022-03-31 21:30:30 -0700

    adds video call, possible bug with audio

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -9,17 +9,4 @@ $showQuestionKebab.click(function() {
 
 
 
-//------Socket.io--------
-
-const socket = io('/') //creating socket connection....connects socket at the root path of our server
-//can i connect it only when I hit a certain path?
-//gotta change this when I deploy
-
-socket.emit('join-room', ROOM_ID, 10); //sends an event to our server
-//10 should be mySDP
-
-
-socket.on('user-connected', userId => {
-    console.log('user connected ' + userId); //another user's SDP should replace userId
-})
 
